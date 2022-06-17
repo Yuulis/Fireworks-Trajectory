08df320cacc8938bd3e26ce93c67b1cab1937d08
Yuulis <yuulis2005@gmail.com>
2022-06-17 14:15:42 +0900

    結果と画像を追加

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from cProfile import label
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 def my_sin(deg):
     if deg == 30 or deg == -30:
@@ -36,15 +39,22 @@ def my_cos(deg):
     elif deg == -180:
         return 0
 
+
 # 初速度 [m/s]
 v0 = 10
 
 # 重力加速度 [m/s^2]
 g = 10
 
+
+fig = plt.figure()
 num = 0
+
 for theta in range(90, -270, -30):
     num += 1
+    
+    xs = np.empty(4, dtype=np.float32)
+    ys = np.empty(4, dtype=np.float32)
     
     for time in range(1, 5):
         x = v0 * my_cos(theta) * time
@@ -53,5 +63,26 @@ for theta in range(90, -270, -30):
         x = round(x, 2)
         y = round(y, 2)
         
-        print(f'({num}) : t = {time}, (x, y) = ({x}, {y})')
+        xs[time - 1] = x
+        ys[time - 1] = y
         
+        print(f'({num}) : t = {time}, (x, y) = ({x}, {y})')
+
+    print(xs)
+    print(ys)
+    
+    plt.plot(xs, ys, label=f'({num})')
+
+
+# グラフ設定
+plt.title('Fireworks Trajectory')
+
+plt.xlabel('x position')
+plt.ylabel('y position')
+plt.xlim(-50, 50)
+plt.ylim(-125, 125)
+plt.grid()
+plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=8)
+
+plt.show()
+fig.savefig('Fireworks Trajectory.png')
